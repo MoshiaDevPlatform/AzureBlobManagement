fbcacb6256256159669a0c14d06bec0f491be3fb
Hector Ace <hectorlindnhle03@gmail.com>
2020-07-14 00:56:57 +0200

    my project

---
+++ ViewModel/PhotoUpload.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.Build.Framework;
+
+namespace ViewModel
+{
+    public class PhotoUpload
+    {
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Please select file.")]
+        public HttpPostedFileBase FileUpload { get; set; }
+    }
+}
